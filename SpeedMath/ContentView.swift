cc244551a69204db7a6974e767e9341a3bc2b724
Nadya Post <nadya@bournedigital.com.au>
2020-03-22 22:12:33 +1100

    Initial Commit

---
+++ SpeedMath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpeedMath
+//
+//  Created by Nadya Post on 22/3/20.
+//  Copyright © 2020 Nadya Post. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

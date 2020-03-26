e450438c1ad604374d508a0d5f53de0385c5b2ee
Nadya Post <nadya@bournedigital.com.au>
2020-03-26 22:45:51 +1100

    UI and logic

--- SpeedMath/ContentView.swift
+++ SpeedMath/ContentView.swift
@@ -7,13 +7,162 @@
 //
 
 import SwiftUI
+enum Position {
+    case answerd, current, upcoming
+}
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+struct Question {
+    let text: String
+    let actualAnswer: String
+    var userAnswer = ""
+    var paddingAmount = 0
+    
+    init() {
+        let left = Int.random(in: 1...10)
+        let right = Int.random(in: 1...10)
+        
+        text = "\(left) + \(right) = "
+        actualAnswer = "\(left + right)"
+        
+        if left < 10 {
+            paddingAmount = 2
+        }
+        if right < 10 {
+            paddingAmount = 2
+        }
+        if left < 100 {
+            paddingAmount = 1
+        }
+        if right < 100 {
+            paddingAmount = 1
+        }
     }
 }
+ 
+struct QuestionRow: View {
+    var question: Question
+    var position: Position
+    
+    var positionColor: Color {
+        switch position {
+        case .answerd:
+            if question.userAnswer == question.actualAnswer {
+                return Color.green.opacity(0.8)
+            } else {
+                return Color.red.opacity(0.8)
+            }
+        case .current:
+            return Color.black.opacity(0.8)
+        case .upcoming:
+            return Color.blue
+        }
+    }
+    var body: some View {
+        HStack {
+            HStack (spacing: 0) {
+                if question.paddingAmount > 0 {
+                    Text(String(repeating: " ", count: question.paddingAmount))
+                }
+                Text(question.text)
+            }
+            .padding([.top, .bottom, .leading])
+            ZStack {
+                Text(" ")
+                .padding()
+                .frame(width: 150)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                    .fill(positionColor)
+                )
+                Text(question.userAnswer)
+            }
+        }
+        .font(.system(size: 48, weight: .regular, design: .monospaced))
+        .foregroundColor(.white)
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var questions = [Question]()
+    @State private var number = 0
+    
+    var score: Int {
+        var total  = 0
+        for i in 0..<number {
+            if questions[i].userAnswer == questions[i].actualAnswer {
+                total += 1
+            }
+        }
+        return total
+    }
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<questions.count, id: \.self) { index in
+                QuestionRow(question: self.questions[index], position: self.position(for: index))
+                    .offset(x: 0, y: CGFloat(index) * 100 - CGFloat(self.number) * 100)
+            }
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Score: \(score)")
+                    .padding()
+                    .background(Capsule().fill(Color.white.opacity(0.8)))
+                    .animation(nil)
+                }
+                .font(.largeTitle)
+                .foregroundColor(.black)
+            .padding()
+                Spacer()
+                
+            }
+        .padding()
+        }
+        .frame(width: 1000, height: 600)
+        .background(LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .onAppear(perform: createQuestion)
+        .onReceive(NotificationCenter.default.publisher(for: .enterNumber)) { note in
+            guard let number = note.object as? Int else { return }
+            
+            if self.questions[self.number].userAnswer.count < 3 {
+                self.questions[self.number].userAnswer += String(number)
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: .removeNumber)) { _ in
+            _ = self.questions[self.number].userAnswer.popLast()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: .submitAnswer)) { _ in
+            if self.questions[self.number].userAnswer.isEmpty == false {
+                withAnimation {
+                    self.number += 1
+                }
+            }
+        }
+    }
+    
+    
+    func createQuestion() {
+        for _ in 1...50 {
+            questions.append(Question())
+        }
+    }
+    func position(for index: Int) -> Position {
+        if index < number {
+            return .answerd
+        } else if index == number {
+            return .current
+        } else {
+            return .upcoming
+        }
+    }
+}
+
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -21,3 +170,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
